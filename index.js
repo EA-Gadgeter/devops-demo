07ee43873d59a253b92526164c6be43cf7c0ab6a
EA-Gadgeter <emiacevedoposos@gmail.com>
2023-07-06 20:26:18 -0600

    Fixed path problems, modified deploy worflow

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import { router } from "./router/index.router";
 
+const appRoute = import.meta.env.VITE_APP_ROUTE;
+
 document.querySelector("body").addEventListener("click", (event) => {
   if (!(event.target.tagName === "A" && event.target.classList.contains("Link"))) return;
 
   event.preventDefault();
 
   const route = event.target.getAttribute("href");
-  window.history.pushState({}, "", route);
+  window.history.pushState({}, "", `${appRoute}${route}`);
   router(route);
 });
 
+
 window.onpopstate = () => {
-  const route = window.location.pathname;
+  const path = window.location.pathname;
+  const route = path.substring(path.lastIndexOf("/"));
+
   router(route);
 };
 
 (() => {
-  const route = window.location.pathname;
+  const path = window.location.pathname;
+  const route = path.substring(path.lastIndexOf("/"));
   router(route);
 })();
